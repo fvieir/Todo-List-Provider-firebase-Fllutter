125235fa6775c0db6e9c7c4f478d8f4707b385ba
Fabricio Dourado <fcvieira2@gmail.com>
2023-06-25 09:14:19 -0300

    Adicionar no provider a conexão

--- lib/app/services/app_modules.dart
+++ lib/app/services/app_modules.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_list_with_provider_firebase/app/core/database/sqlite_connection_factory.dart';
 import 'package:todo_list_with_provider_firebase/app/services/app_widget.dart';
 
 class AppModules extends StatefulWidget {
@@ -14,7 +15,10 @@ class _AppModulesState extends State<AppModules> {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        Provider(create: (context) => Object()),
+        Provider(
+          create: (context) => SqliteConnectionFactory(),
+          lazy: false,
+        ),
       ],
       child: const AppWidget(),
     );
