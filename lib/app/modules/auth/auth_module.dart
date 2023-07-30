285778d2a56974fe119472252af3b6d5e875f4a4
Fabricio Dourado <fcvieira2@gmail.com>
2023-07-30 09:53:15 -0300

    Adicionar pagina de cadsatro

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -2,6 +2,8 @@ import 'package:provider/provider.dart';
 import 'package:todo_list_with_provider_firebase/app/core/modules/todo_list_modules.dart';
 import 'package:todo_list_with_provider_firebase/app/modules/auth/login/login_controller.dart';
 import 'package:todo_list_with_provider_firebase/app/modules/auth/login/login_page.dart';
+import 'package:todo_list_with_provider_firebase/app/modules/auth/register/register_controller.dart';
+import 'package:todo_list_with_provider_firebase/app/modules/auth/register/register_page.dart';
 
 class AuthModule extends TodoListModules {
   AuthModule()
@@ -10,9 +12,13 @@ class AuthModule extends TodoListModules {
             ChangeNotifierProvider(
               create: (context) => LoginController(),
             ),
+            ChangeNotifierProvider(
+              create: (context) => RegisterController(),
+            ),
           ],
           routes: {
             '/login': (context) => const LoginPage(),
+            '/register': (context) => const RegisterPage(),
           },
         );
 }
